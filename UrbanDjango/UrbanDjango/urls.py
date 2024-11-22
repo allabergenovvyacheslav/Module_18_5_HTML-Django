9920d4ca02896470110f6fc67c1d6790c79944d6
Vyacheslav <slawushkin@mail.ru>
2024-11-22 17:00:48 +0300

    module_18_2(домашнее задание 18_2)

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', ClassTemplate.as_view()),
+    path('func/', func_template)
 ]
